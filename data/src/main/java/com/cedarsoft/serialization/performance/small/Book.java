0513b4c35becb362ad14d2f4f856ec5fd32896f6
Johannes Schneider <js@cedarsoft.com>
2014-11-05 22:19:30 +0100

    added Book

---
+++ data/src/main/java/com/cedarsoft/serialization/performance/small/Book.java
@@ -0,0 +1,64 @@
+package com.cedarsoft.serialization.performance.small;
+
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class Book {
+  @Nonnull
+  private final String title;
+  @Nonnull
+  private final List<String> authors;
+  private final double price;
+  @Nonnull
+  private final Rating rating;
+
+  public Book( @Nonnull String title, @Nonnull List<String> authors, double price, @Nonnull Rating rating ) {
+    this.title = title;
+    this.authors = ImmutableList.copyOf( authors );
+    this.price = price;
+    this.rating = rating;
+  }
+
+  @Nonnull
+  public String getTitle() {
+    return title;
+  }
+
+  @Nonnull
+  public List<String> getAuthors() {
+    //noinspection ReturnOfCollectionOrArrayField
+    return authors;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  @Nonnull
+  public Rating getRating() {
+    return rating;
+  }
+
+  public enum Rating {
+    ONE( 1 ),
+    TWO( 2 ),
+    THREE( 3 ),
+    FOUR( 4 ),
+    FIVE( 5 );
+    private final int stars;
+
+    Rating( int stars ) {
+      this.stars = stars;
+    }
+
+    public int getStars() {
+      return stars;
+    }
+  }
+
+}
