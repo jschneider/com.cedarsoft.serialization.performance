54402fe34bf01247c235275881fce1025a9f5e60
Johannes Schneider <js@cedarsoft.com>
2014-11-07 18:04:45 +0100

    prepared base class for book serialization

--- base/src/main/java/com/cedarsoft/serialization/performance/small/AbstractBookSerializeRunner.java
+++ base/src/main/java/com/cedarsoft/serialization/performance/small/AbstractBookSerializeRunner.java
@@ -1,9 +1,65 @@
 package com.cedarsoft.serialization.performance.small;
 
+import com.cedarsoft.serialization.performance.AbstractRunner;
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
-public class AbstractBookSerializeRunner {
+public abstract class AbstractBookSerializeRunner extends AbstractRunner {
+  @Nonnull
+  private final ScenarioSize scenarioSize;
+
+  protected AbstractBookSerializeRunner( @Nonnull ScenarioSize scenarioSize ) {
+    this.scenarioSize = scenarioSize;
+  }
+
+  public void runDeserializationBenchmark() {
+    Book book = new Book( "Design Patterns: Elements of Reusable Object-Oriented Software",
+                          ImmutableList.of( "Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides" ),
+                          38.02, Book.Rating.FOUR );
+
+    byte[] serialized = serializeBook( book );
 
 
+    runBenchmark( () -> {
+      Book deserialized = deserializeBook( serialized );
+      if ( !deserialized.getTitle().equals( book.getTitle() ) ) {
+        throw new IllegalStateException( "Different title. Was <" + deserialized.getTitle() + "> but expected <" + book.getTitle() + ">" );
+      }
+    }, scenarioSize.getCount() );
+  }
+
+  /**
+   * Serializes the book
+   *
+   * @param book the book that shall be serialized
+   * @return the bytes
+   */
+  protected abstract byte[] serializeBook( @Nonnull Book book );
+
+  /**
+   * Deserializes the book
+   *
+   * @param serialized the serialized data
+   */
+  @Nonnull
+  protected abstract Book deserializeBook( @Nonnull byte[] serialized );
+
+  public enum ScenarioSize {
+    SMALL( 10000 ),
+    MEDIUM( 10000 * 10 ),
+    LARGE( 10000 * 100 );
+    private final int count;
+
+    ScenarioSize( int count ) {
+      this.count = count;
+    }
+
+    public int getCount() {
+      return count;
+    }
+  }
 }
